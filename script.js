e08ee3ab1eee75c7713716f918d650695c152d51
Bebo41 <dokabanoub@gmail.com>
2023-06-04 11:30:29 +0300

    project done

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 //pounds to kilograms
 function convert() {
     var par = document.getElementById("para");
-    var num = document.getElementById("inp").Value;
-    var result = num * 2.2046226218;
+    var num = document.getElementById("inp").value;
+    result = num * 2.2046226218;
     par.innerHTML = result;
     console.log("function one called")
     console.log("Result: " + result);
+};
+//kilograms to pounds
+function convert2() {
+    var par2 = document.getElementById("para2");
+    var num2 = document.getElementById("inp2").value;
+    result2 = num2 / 2.2046226218;
+    par2.innerHTML = result2;
+    console.log("function two called")
+    console.log("Result: " + result2);
 };
